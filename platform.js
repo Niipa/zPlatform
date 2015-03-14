19593bff0b64012d43ca4385597e567a942121d0
Menard (Marty) Soliven <mszsoliven@live.com>
2015-03-14 18:10:28 -0400

    Update 3/14/15

--- platform.js
+++ platform.js
@@ -1,37 +1,52 @@
-// Copyright (c) 2015 Menard Z. Soliven
+// Copyright (c) 2015 Menard (Ren) Z. Soliven
 // Distributed under the MIT software license
 // See license.txt or http://www.opensource.org/licenses/mit-license.php
 
 module.exports = function(app){
   var mongo = require('mongodb');
-  var monk = require('monk');
   var express = require('express');
-  var sanitizer = require('santizer');
-  var fs = require('fs');
+  var sanitizer = require('sanitizer');
 
-  var db = monk('view1:viewingTheWorld@localhost:27891', function(err){
-    if(err){
-      /* TODO: logToFile() */
-      console.error(err); 
-    }
-  });
-
-  var _collection = db.get('usrs');
-
-  app.use(express.static(_dirname+'/resources',
-      {
-        'welcome':['welcome.html']
+  var jade = require('jade');
+  var htmlFactory = jade.compileFile('resources/welcome.jade');
+  var db = require('monk')('localhost:27891/zelDB', 
+    function(err){
+      if(err){
+        /* TODO: logToFile() */
+        console.error(err); 
       }
-    )
+    }
   );
 
+  var _collection = db.get('users');
+
+  // Serving Logic
+  app.use(express.static(__dirname + '/resources'));
+  var sendOpts = {
+    maxAge : '15 days',
+    lastModified : true,
+    dotfiles :  "ignore"
+  };
+  var sendFileCallBack = function(err){
+    if(err){
+      console.error(err);
+      res.status(err.status).end();
+    }else{
+      console.log('Sent successfully.');
+    }
+  };
+
   app.use(function(req, res, next){
+
     switch(req.url){
-      case '/':
-        var user = sanitizer.santize(req.body.u);
+      case '/00559ea764f3549e2a9c714ecd8af73f':
+
+        var user = sanitizer.sanitize(req.body.u);
         var password = sanitizer.sanitize(req.body.p);
 
-        coll.count({'username':user, 'password':password}, function(err, doc){
+        console.log(user + ':' + password);
+
+        _collection.count({'usr':user, 'pw':password}, function(err, doc){
           if(err){
             res.status(500).send("Internal error.");
             
@@ -44,19 +59,28 @@ module.exports = function(app){
               sess.usr = user;
               sess.password = password;
 
+              // Session Fixation Defense
               req.session.regenerate(function(err){
-                if(err)
                   /* TODO: logToFile() */
               });
 
-              res.location("resources/welcome.html");
-              res.redirect("resources/welcome.html");
+              // Generate welcome.html
+              // TODO: appList logic.
+              var locals = {usr: user}
+              // Generate html, send html.
+              res.send(htmlFactory(locals));
+
             }else{
-              res.location("resources/reject.html");
-              res.redirect("resources/reject.html");
+              res.sendFile(__dirname + "/resources/reject.html", sendOpts, 
+                sendFileCallBack);
             }
           }
         });
+      break;
+      default:
+        res.sendFile(__dirname + "/resources/reject.html", sendOpts, 
+          sendFileCallBack);
+      break;
     }
   });
 }
---
+++ readme.md.txt
@@ -0,0 +1,1 @@
+Dashboard to mount Web Applications to.
